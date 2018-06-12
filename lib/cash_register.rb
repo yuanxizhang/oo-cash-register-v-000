b2c3150d88b093505b5dd737b525616074c39974
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 22:35:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,9 @@ class CashRegister
   
   def add_item(title, price, quantity = 1) 
     @total += price * quantity
-    @items << title
+    quantity.times do
+        @items << title
+    end
   end 
   
   def items
