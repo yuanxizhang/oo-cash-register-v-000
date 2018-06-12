2ef9c306603a4b5e03bccd5893607500777017e0
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 22:21:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,12 +17,13 @@ class CashRegister
   
   def apply_discount 
     if @discount == 20
-    
       discount_amount = (@total * @discount)/100.00
       @total = @total - discount_amount
       return "After the discount, the total comes to $#{@total.to_i}."
     elsif @discount == 0 
-      
+      discount_amount = 0
+      @total = @total - discount_amount
+      return "There are no discount to apply."
       
     end
   end 
