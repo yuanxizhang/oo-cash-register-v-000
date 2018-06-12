c775ce6ec949577f2bf6f19952669ef94da9fbbe
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 21:38:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
-
+# Build the CashRegister class 
+def CashRegister 
+  attr_accessor :item :count :price
+  @@total = 0.0
+  
+  def initialize 
+    @@total << @count * @prince 
+  end 
+  
+  def 
